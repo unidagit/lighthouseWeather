083cdb27d1b81ae8c99fc50a302971e2644074fb
unidagit <rhcjrdbsgml@gmail.com>
2023-05-10 08:27:34 +0900

    Feat : Jeju page 생성

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+// import { Inter } from "next/font/google";
+// const inter = Inter({ subsets: ["latin"] });
+
+import JejuInfo from "./components/jejuInfo/JejuInfo";
+
+export default function JejuHomePage() {
+  return <JejuInfo />;
+}
